6a0627fd1e1bbe256b8d351fa51023f21950306d
jongwon_Kim <jw909069@naver.com>
2025-05-12 21:38:01 +0900

    링크 다시 변경

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const WEDDING_INVITATION_URL = "http://localhost:8080/";
+export const WEDDING_INVITATION_URL = "https://astonishing-dodol-3ee41b.netlify.app/";
 export const KAKAOTALK_API_TOKEN = "97d4d056de9226d475737055879a1171";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
